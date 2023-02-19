9a6b70cd8c5a4298219dfef8c91af2183010f51d
lazezdravev <lazezdravev30@gmail.com>
2023-02-19 19:57:10 +0100

    Implement Dashboard Template

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+
+{
+
+public function __construct()
+{
+    $this->middleware('auth');
+}
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
+        $data = ['users' => $users];
+       return view('dashboard.users.index')->with($data);
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('dashboard.users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+
+        $name = $request->get('name');
+        $email = $request->get('email');
+        $password = bcrypt($request->get('password'));
+
+
+        User::create([
+            "name" => $name,
+            "email" => $email,
+            "password" => $password,
+
+        ]);
+
+        return redirect()->route('users.index');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::FindOrFail($id);
+        $data = ['user' => $user];
+        return view('dashboard.users.show')->with($data);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = User::FindOrFail($id);
+
+
+        $data = ['user' => $user];
+
+        return view('dashboard.users.edit')->with($data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = User::FindOrFail($id);
+
+        $user->fill($request->all())->save();
+
+        return redirect()->route('users.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = User::FindOrFail($id);
+        $user->delete();
+        return redirect()->route('users.index');
+    }
+
+}
